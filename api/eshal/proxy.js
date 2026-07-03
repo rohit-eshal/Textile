bc52ba717de4d34ecd3492abc070599ce65850e9
harshit-eshal <harshit.vyas@eshal.ai>
2026-07-03 13:45:03 +0530

    feat: add launch configuration and update @eshal-bot/server to version 0.1.8

--- api/eshal/proxy.js
+++ api/eshal/proxy.js
@@ -1,8 +1,14 @@
 import { createEshalProxy } from '@eshal-bot/server'
 
-const proxy = await createEshalProxy({
-  secretKey:   process.env.ESHAL_SECRET_KEY,
-  eshalApiUrl: process.env.ESHAL_API_URL,
-})
+let handler
 
-export default proxy.vercel()
+export default async function (req, res) {
+  if (!handler) {
+    const proxy = await createEshalProxy({
+      secretKey:   process.env.ESHAL_SECRET_KEY,
+      eshalApiUrl: process.env.ESHAL_API_URL,
+    })
+    handler = proxy.vercel()
+  }
+  return handler(req, res)
+}
